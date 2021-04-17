bf6aa0e587ac19e32fcbdc25485d0908fb01ba1c
Joshua Sells <joshuataylorsells@gmail.com>
2021-04-17 11:22:04 -0500

    cleans up the javascript files

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,17 @@
 import React, {Component} from 'react';
-import Master from './pages/Master';
+import Navbar from './micro-components/Navbar';
+import Login from './micro-components/Login';
+import Header from './micro-components/Header';
+import Footer from './micro-components/Footer';
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
-        <Master />
+        <Navbar />
+        <Header />
+        <Footer />
       </div>
     );
   }
